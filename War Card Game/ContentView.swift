b10a4fa40994be016344cdbd3befbf88b58cc2b6
Ricardo Camarena <ricky_camarena@hotmail.com>
2023-04-06 01:23:12 -0600

    feat: Added elements to the view

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -8,14 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var playerCard = "card2"
+    var cpuCard = "card3"
+    
+    var playerScore = 0
+    var cpuScore = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Image("logo")
+                    .resizable()
+                    .frame(width: 122*1.5, height: 67*1.5)
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(cpuCard)
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Button {
+                    deal()
+                } label: {
+                    Image("dealbutton")
+                        .resizable()
+                        .frame(width: 82*2.5, height: 31*2.5)
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .padding(.bottom, 10)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .padding(.bottom, 10)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                    }
+                    
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                Spacer()
+            }
         }
-        .padding()
+    }
+    
+    func deal() {
+        print("Deal cards")
     }
 }
 
