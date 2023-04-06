af0c248d6505abfdc4b1b08929fd3323320ffbb1
Ricardo Camarena <ricky_camarena@hotmail.com>
2023-04-05 23:56:26 -0600

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Ricardo Camarena on 05/04/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
